1c03acba36b8226e2af26f77e909aa37a230f6f7
tedirland <irlandth@gmail.com>
2024-03-01 15:34:29 -0600

    change run method

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
         search(&config.query, &contents)
     };
 
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}")
     }
     Ok(())
@@ -50,11 +50,13 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
+
     for line in contents.lines() {
         if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
+
     results
 }
 
