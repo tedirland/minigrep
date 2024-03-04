0d169082c70d09a61fa7cdce4406ad82ad8d5b95
tedirland <irlandth@gmail.com>
2024-03-04 07:41:05 -0600

    refactor cli to print errors out to the standard error stream and write output to a text file

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing command line arguments: {err}");
+        eprintln!("Problem parsing command line arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(cfg) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
